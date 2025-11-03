51be0d8549bdc27811cd944efc229ca991158c43
Kajal <fhkajal.ouritspace@gmail.com>
2025-11-03 18:05:55 +0600

    fix

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -6,7 +6,7 @@ import { axiosBaseQuery } from "@/helpers/axios/axiosBaseQuery";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: axiosBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1",
+    baseUrl: "https://job-task-2-backend.vercel.app/api/v1",
   }),
   endpoints: () => ({}),
   tagTypes: tagTypesList,
