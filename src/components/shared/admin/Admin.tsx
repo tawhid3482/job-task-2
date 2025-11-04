763158310960996fc075bd6ce7b7f604a1bc119b
Tawhidul Islam <138350536+tawhid3482@users.noreply.github.com>
2025-11-04 21:15:04 +0600

    Save local changes

--- src/components/shared/admin/Admin.tsx
+++ src/components/shared/admin/Admin.tsx
@@ -1,6 +1,4 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable react/no-unescaped-entities */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 // app/admin/dashboard/page.tsx
 "use client";
 import { useEffect, useState } from "react";
@@ -136,7 +134,7 @@ const AdminDashboard = () => {
   }
 
   return (
-    <div className="flex h-screen bg-gray-100">
+    <div className="flex h-screen bg-gray-100 md:my-24 ">
       {/* Sidebar */}
       <div className="bg-gray-800 text-white w-64 space-y-6 py-7 px-2 absolute inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0 transition duration-200 ease-in-out">
         <div className="text-white flex items-center space-x-2 px-4">
@@ -172,10 +170,6 @@ const AdminDashboard = () => {
             Testimonial ({stats.testimonialsCount})
           </Link>
 
-         
-
-         
-
           {/* <div className="pt-4">
             <p className="px-4 text-xs font-semibold text-gray-400 uppercase">
               Customer Support
@@ -202,27 +196,6 @@ const AdminDashboard = () => {
             </div>
 
             <div className="flex items-center space-x-4">
-              <div className="relative">
-                <input
-                  type="text"
-                  placeholder="Q Search"
-                  className="pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-                <svg
-                  className="w-5 h-5 absolute left-3 top-2.5 text-gray-400"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                  />
-                </svg>
-              </div>
-
               <button
                 onClick={handleLogout}
                 className="bg-red-600 text-white px-4 py-2 rounded-lg hover:bg-red-700 transition-colors"
