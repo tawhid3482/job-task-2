e5005ac1fd1d69ddaaae4f87b7dd87b04790d5ce
Mdshahriadristy <157006146+Mdshahriadristy@users.noreply.github.com>
2025-11-13 15:20:34 +0600

    fix

--- src/app/(withCommonLayout)/login/page.tsx
+++ src/app/(withCommonLayout)/login/page.tsx
@@ -15,7 +15,7 @@ const LoginPage = () => {
     setError('');
 
     try {
-      const response = await fetch('http://160.25.7.224:5008/api/v1/auth/login', {
+      const response = await fetch('https://assistholdingsltd.com/api/api/v1/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
