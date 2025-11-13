0069e47987815c46522e1297e299d93e70fb5981
Tawhidul Islam <138350536+tawhid3482@users.noreply.github.com>
2025-11-13 14:39:33 +0600

    Update baseApi.ts

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -6,7 +6,7 @@ import { axiosBaseQuery } from "@/helpers/axios/axiosBaseQuery";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: axiosBaseQuery({
-    baseUrl: "http://160.25.7.224:5008/api/v1",
+    baseUrl: "https://assistholdingsltd.com/api/api/v1",
   }),
   endpoints: () => ({}),
   tagTypes: tagTypesList,
