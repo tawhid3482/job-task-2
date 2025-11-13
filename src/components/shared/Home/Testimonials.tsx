9c6062dbcdf969128947ec2b38ea753a7d6938c4
Tawhidul Islam <138350536+tawhid3482@users.noreply.github.com>
2025-11-13 14:43:51 +0600

    Update Testimonials.tsx

--- src/components/shared/Home/Testimonials.tsx
+++ src/components/shared/Home/Testimonials.tsx
@@ -36,7 +36,7 @@ const Testimonials: React.FC = () => {
     const fetchTestimonials = async () => {
       try {
         const res = await fetch(
-          `http://160.25.7.224:5008/api/v1/testimonial`
+          `https://assistholdingsltd.com/api/api/v1/testimonial`
         );
         const json = await res.json();
         setTestimonials(Array.isArray(json.data) ? json.data : []);
